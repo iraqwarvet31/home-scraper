538949b31a2564c030d73856237a51016f91b0ff
Larry Olguin <iraqwarvet31@gmail.com>
2021-02-13 11:05:02 -0800

    add webpack boilerplate

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3000;
+// const db = require('../database');
+
+const app = express();
+const CLIENT_DIR = path.resolve(__dirname, '..', 'client', 'dist');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(CLIENT_DIR));
+
+// create request to /api/groceries endpoint and query our database and respond with data to client
+// app.get('/api/groceries', (req, res) => {
+//   const sql = 'SELECT * FROM groceries';
+//   db.query(sql, (err, results) => {
+//     if (err) {
+//       console.log(err);
+//       res.sendStatus(500);
+//     } else {
+//       res.send(results);
+//     }
+//   })
+// })
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+})
+
