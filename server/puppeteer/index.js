b2bbe3e5a461a2ee103eb8ab5884506f7583830b
Larry Olguin <iraqwarvet31@gmail.com>
2021-02-14 00:17:59 -0800

    add puppeteer and script

---
+++ server/puppeteer/index.js
@@ -0,0 +1,56 @@
+const preparePageForTests = async (page) => {
+    // Pass the User-Agent Test
+    const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
+      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+    await page.setUserAgent(userAgent);
+}
+
+(async () => {
+  var counter = 1;
+  // Extract partners on the page, recursively check the next page in the URL pattern
+  const extractApartments = async url => {
+    const page = await browser.newPage();
+    await preparePageForTests(page);
+    await page.goto(url);
+    // Scrape the data we want
+    const apartmentsOnPage = await page.evaluate(() => {
+      let apartmentsArr = [];
+      let apartments = document.querySelectorAll('.copyContainer');
+      let pageNumbers = document.querySelectorAll('a[data-page]')
+      apartments.forEach(element => {
+        let apartmentObj = {};
+        let name = element.childNodes[1].innerText;
+        let address = element.childNodes[3].innerText;
+        let price = element.childNodes[7].childNodes[1].childNodes[1].innerText;
+        let size = element.childNodes[7].childNodes[1].childNodes[3].innerText;
+
+        apartmentObj['Name'] = name;
+        apartmentObj['Address'] = address;
+        apartmentObj['Price'] = price;
+        apartmentObj['Size'] = size;
+        apartmentsArr.push(apartmentObj);
+      })
+      return apartmentsArr;
+    })
+    counter++
+    if (counter === 3) {
+      // Last page scraped terminate
+      return apartmentsOnPage
+    } else {
+      const nextUrl = `https://www.apartmentfinder.com/California/Los-Angeles-Apartments/Page${counter}`
+      return apartmentsOnPage.concat(await extractApartments(nextUrl))
+    }
+  };
+
+  const browser = await puppeteer.launch({
+        args: ['--no-sandbox'],
+        headless: true,
+  });
+  const firstUrl =
+    "https://www.apartmentfinder.com/California/Los-Angeles-Apartments/Page1"
+  const apartments = await extractApartments(firstUrl)
+
+  console.log(apartments);
+
+  await browser.close();
+})();
